c3407f78b7cef3ca5998565432835ad3f04bc739
evan chime <evanchime@yahoo.co.uk>
2018-04-25 17:26:19 +0100

    changed #define no_of_bits to a portable constexpr

--- binary_problem.cpp
+++ binary_problem.cpp
@@ -9,20 +9,29 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
+#include <limits>
 
 using namespace std;
 
-#define no_of_bits static_cast<int>(ceil(log2(1000)))
+//change numeric_limits template parameter accordingly 
+#define no_of_bits numeric_limits<int>::digits
 
 int main (int argc, const char *argv[]){
 
     vector<int>coll = {6,6,2,19,45,3,0};
+    //user defined comp function used as third argument to sort 
     sort (coll.begin(), coll.end(), [](int a, int b){
         size_t ones_in_a = bitset<no_of_bits>(a).count(), ones_in_b = bitset<no_of_bits>(b).count();
         return (ones_in_a == ones_in_b) ? (a < b) : (ones_in_a < ones_in_b);
     });
-    for (const auto x : coll){
+    //range based for loop to check vector was sorted
+    for (const auto &x : coll){
         cout << x << endl;
     }
+    
     return 0;
 }
+
+
+
+
